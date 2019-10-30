df36884369ccce5dbba45502f2fe1f9bb385a418
Gokul <gokulrajxa@gmail.com>
2019-10-30 16:41:31 +0530

    facebook

--- myapp/views.py
+++ myapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 def home(request):
     return render(request,'home.html')
 def page1(request):
-    return render(request,page.html)
+    return render(request,'page.html')
